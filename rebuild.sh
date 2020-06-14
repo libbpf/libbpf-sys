23c215bf16b1ce51d7da46aaf61a2f997d0b0e8c
Alex Forster <alex@alexforster.com>
2020-06-14 14:32:00 -0500

    Minor rebuild.sh fixups

--- rebuild.sh
+++ rebuild.sh
@@ -4,21 +4,22 @@ DOCKER="docker"
 
 set -eu -o pipefail
 
-${DOCKER} build --no-cache -t libbpf-sys-builder - <<'EOF'
+${DOCKER} build -t libbpf-sys-builder - <<'EOF'
 FROM amd64/ubuntu:bionic AS libbpf-sys-builder
 
-ENV LANG=C.UTF-8
-ENV LC_ALL C.UTF-8
+ENV LANG=C.UTF-8 \
+    LC_ALL=C.UTF-8
 
 VOLUME /usr/local/src/libbpf-sys
 WORKDIR /usr/local/src/libbpf-sys
 
-SHELL ["/bin/bash", "-e", "-u", "-o", "pipefail", "-c"]
+SHELL ["/bin/bash", "-eu", "-o", "pipefail", "-c"]
 
 RUN \
-	DEBIAN_FRONTEND=noninteractive apt-get -q update; \
-	DEBIAN_FRONTEND=noninteractive apt-get -q install -y curl build-essential linux-headers-generic zlib1g-dev libelf-dev libclang-dev llvm clang pkg-config; \
-	DEBIAN_FRONTEND=noninteractive apt-get -q clean autoclean;
+	export DEBIAN_FRONTEND=noninteractive; \
+	apt-get -q update; \
+	apt-get -q install -y curl build-essential linux-headers-generic zlib1g-dev libelf-dev libclang-dev llvm clang pkg-config; \
+	apt-get -q clean autoclean;
 
 RUN \
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
@@ -35,7 +36,6 @@ ENTRYPOINT \
 		--whitelist-function "libbpf_.+" \
 		--whitelist-function "xsk_.+" \
 		--whitelist-function "_xsk_.+" \
-		--whitelist-function "xdp_.+" \
 		--whitelist-function "perf_buffer_.+" \
 		--whitelist-type "bpf_.*" \
 		--whitelist-type "xdp_.*" \
