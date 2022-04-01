793c60bf8016b4c19f19b71d85c4005739f92930
Milan <milan@mdaverde.com>
2022-03-31 20:38:18 -0400

    Updates rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -24,41 +24,8 @@ RUN \
 RUN \
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable;
 
-RUN \
-	source $HOME/.cargo/env; \
-	cargo install --git https://github.com/rust-lang/rust-bindgen --rev 2aed6b0 bindgen;
-
 ENTRYPOINT \
 	source $HOME/.cargo/env; \
-	bindgen \
-		--verbose \
-		--with-derive-default \
-		--explicit-padding \
-		--whitelist-function "bpf_.+" \
-		--whitelist-function "btf_.+" \
-		--whitelist-function "libbpf_.+" \
-		--whitelist-function "xsk_.+" \
-		--whitelist-function "_xsk_.+" \
-		--whitelist-function "perf_buffer_.+" \
-		--whitelist-function "ring_buffer_.+" \
-		--whitelist-type "bpf_.*" \
-		--whitelist-type "btf_.*" \
-		--whitelist-type "xdp_.*" \
-		--whitelist-type "xsk_.*" \
-		--whitelist-var "BPF_.+" \
-		--whitelist-var "BTF_.+" \
-		--whitelist-var "XSK_.+" \
-		--whitelist-var "XDP_.+" \
-		--default-enum-style consts \
-		--no-prepend-enum-name \
-		--no-layout-tests \
-		--no-doc-comments \
-		--builtins \
-		--output $(pwd)/src/bindings.rs \
-		$(pwd)/bindings.h \
-		-- \
-		-I$(pwd)/libbpf/include \
-		-I$(pwd)/libbpf/include/uapi; \
 	cargo build --release --verbose;
 EOF
 
